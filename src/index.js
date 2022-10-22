abb833cefafaa1e403722e6d004b37aeccb7df5d
varinda98 <varindakataria98@gmail.com>
2022-10-22 15:09:12 +0530

    db connection

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
+mongoose.connect("mongodb+srv://varinda:Flipkart@newproject.7qwzr8u.mongodb.net/test"
 , {
    useNewUrlParser: true 
 }
