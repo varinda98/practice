f655a197c085fb4e9b8b0d39973297e44ac80484
varinda98 <varindakataria98@gmail.com>
2022-11-03 22:46:35 +0530

    31oct assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,6 @@
 const UserModel= require("../models/userModel")
 
 
-
-
 const basicCode= async function(req, res) {
     let tokenDataInHeaders= req.headers.token
     console.log(tokenDataInHeaders)
@@ -12,23 +10,15 @@ const basicCode= async function(req, res) {
     res.send({ msg: "This is coming from controller (handler)"})
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const getIpAdress = async function (req,res) {
+    res.send({msg: "this is for IP Adress"})
+}
+const getCurrentTime = async function (req,res){
+    res.send ({msg : "this is For Current Time"})
+}
+const getPath = async function (req,res){
+    res.send ({msg: "this is for GetPath details"})
+}
 
 const createUser= async function (req, res) {
     let data= req.body
@@ -44,3 +34,6 @@ const getUsersData= async function (req, res) {
 module.exports.createUser= createUser
 module.exports.getUsersData= getUsersData
 module.exports.basicCode= basicCode
+module.exports.getCurrentTime= getCurrentTime
+module.exports.getIpAdress= getIpAdress
+module.exports.getPath= getPath
