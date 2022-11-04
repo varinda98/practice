013710a442f657560d79ca510f3f7b9d9bf979ea
varinda98 <varindakataria98@gmail.com>
2022-11-04 10:06:16 +0530

    incomplete assignment, I will complete in next commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,15 +4,21 @@ const userModel = require("../models/userModel");
 /*
   Read all the comments multiple times to understand why we are doing what we are doing in login api and getUserData api
 */
-const createUser = async function (abcd, xyz) {
-  //You can name the req, res objects anything.
-  //but the first parameter is always the request 
-  //the second parameter is always the response
-  let data = abcd.body;
-  let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
-};
+// const createUser = async function (abcd, xyz) {
+//   //You can name the req, res objects anything.
+//   //but the first parameter is always the request 
+//   //the second parameter is always the response
+//   let data = abcd.body;
+//   let savedData = await userModel.create(data);
+//   console.log(abcd.newAtribute);
+//   xyz.send({ msg: savedData });
+// };
+
+const createUser = async function(req, res){
+      let data = req.body
+      let savedData = await userModel.create(data)
+      res.send(savedData)
+}
 
 const loginUser = async function (req, res) {
   let userName = req.body.emailId;
